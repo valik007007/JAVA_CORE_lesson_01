3a5effbc1e38273e8b7b830726b378889d226bc4
ValikTarasiuk <valiktarasiuk23@gmail.com>
2021-06-14 21:46:50 +0300

    added first project to github java

---
+++ Homework_lesson_01/src/homework_01/Application.java
@@ -0,0 +1,64 @@
+//package homework_01;
+//
+//public class Application {
+//	public static void main(String[] args) {
+//		
+//		//Завдання 1
+//		
+//		byte b = 1;
+//		short sh = 2;
+//		int i = 3;
+//		long l = 4l;
+//		double d = 5;
+//		float f = 6f;
+//		boolean bl = true;
+//		char ch = 22;
+//		
+//		//Завдання 2
+//		
+//		// Byte
+//		System.out.println("Byte max = " + Byte.MAX_VALUE);
+//		System.out.println("Byte min = " + Byte.MIN_VALUE);
+//		
+//		//Short
+//		System.out.println("Short max = " + Short.MAX_VALUE);
+//		System.out.println("Short min= " + Short.MIN_VALUE);
+//		
+//		//Integer
+//		System.out.println("Integer max = " + Integer.MAX_VALUE);
+//		System.out.println("Integer min = " + Integer.MIN_VALUE);
+//		
+//		// Long
+//		System.out.println("Long max = " + Long.MAX_VALUE);
+//		System.out.println("Long min = " + Long.MIN_VALUE);
+//		
+//		// Double
+//		System.out.println("Double max = " + Double.MAX_VALUE);
+//		System.out.println("Double min = " + Double.MIN_VALUE);
+//		
+//		// Float
+//		System.out.println("Float max = " + Float.MAX_VALUE);
+//		System.out.println("Float min = " + Float.MIN_VALUE);
+//		
+//		// Character
+//		System.out.println("Character max = " + Character.MAX_VALUE);
+//		System.out.println("Character min = " + Character.MIN_VALUE);
+//		
+//		// Завдання 3
+//		
+//		int numbers[] = { 92, -100, 99, 120, 67, 4, -9, 55, 11110, 1 };
+//	    int min, max;
+//	    
+//	    min = max = numbers[0];
+//
+//	    for (int j = 1; j < 10; j++) {
+//	        if (numbers[j] < min)
+//	            min = numbers[j];
+//	        if(numbers[j] > max){
+//	            max = numbers[j];
+//	        }
+//	    }
+//	    System.out.println("min is: " + min + "; max is: " + max);
+//	}
+//
+//}
